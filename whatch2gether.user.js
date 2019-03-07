ae7e0affdc1571e4abb22ecd381c44803c03a47e
xPumaa <35280763+xPumaa@users.noreply.github.com>
2019-03-07 23:59:33 +0100

    Update whatch2gether.user.js

--- whatch2gether.user.js
+++ whatch2gether.user.js
@@ -5,7 +5,7 @@
 // @author       xPumaa
 // @license      GPL-3.0; http://www.gnu.org/licenses/gpl-3.0.txt
 // @namespace    https://github.com/xPumaa/whatch2gether-inline-volume
-// @updateURL	   https://github.com/xPumaa/whatch2gether-inline-volume/raw/master/whatch2gether.user.js
+// @updateURL    https://github.com/xPumaa/whatch2gether-inline-volume/raw/master/whatch2gether.user.js
 // @icon         https://www.watch2gether.com/static/w2g.ico
 // @match        http*://www.watch2gether.com/rooms/*
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
