8e7b3cef665058a46900be3282ad463d3bd20091
xPumaa <35280763+xPumaa@users.noreply.github.com>
2020-07-30 23:09:48 +0200

    Added Domain without www

--- whatch2gether.user.js
+++ whatch2gether.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Whatch2Gether inline volume
 // @description  Brings the volume bar into the player settings.
-// @version      1.1.2
+// @version      1.1.3
 // @author       xPumaa
 // @license      GPL-3.0; http://www.gnu.org/licenses/gpl-3.0.txt
 // @namespace    https://github.com/xPumaa/whatch2gether-inline-volume
 // @updateURL    https://github.com/xPumaa/whatch2gether-inline-volume/raw/master/whatch2gether.user.js
 // @icon         https://www.w2g.tv/static/w2g.ico
 // @match        http*://www.w2g.tv/rooms/*
+// @match        http*://w2g.tv/rooms/*
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
